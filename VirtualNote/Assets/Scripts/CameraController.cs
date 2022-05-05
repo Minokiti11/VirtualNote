f668f908afcf649b04bfea37595afa62e24c0815
Mino-rex <macminosp@icloud.com>
2022-05-05 14:40:42 +0900

    We can switch players

--- VirtualNote/Assets/Scripts/CameraController.cs
+++ VirtualNote/Assets/Scripts/CameraController.cs
@@ -4,33 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject mainCamera;              //メインカメラ格納用
     public GameObject playerObject;            //回転の中心となるプレイヤー格納用
     public float rotateSpeed = 2.0f;            //回転の速さ
- 
-    //呼び出し時に実行される関数
-    void Start()
-    {
-        //メインカメラを取得
-        mainCamera = Camera.main.gameObject;
-    }
- 
- 
-    //単位時間ごとに実行される関数
+
     void Update()
     {
         //rotateCameraの呼び出し
-        rotateCamera();
+        rotatePlayer();
     }
  
-    //カメラを回転させる関数
-    private void rotateCamera()
+    //プレイヤーを回転させる関数
+    private void rotatePlayer()
     {
         //Vector3でX,Y方向の回転の度合いを定義
         Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed, 0);
  
-        //transform.RotateAround()をしようしてメインカメラを回転させる
-        mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
-        mainCamera.transform.RotateAround(playerObject.transform.position, transform.right, angle.y);
+        //transform.RotateAround()を使用してメインカメラを回転させる
+        playerObject.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
+        // playerObject.transform.RotateAround(playerObject.transform.position, transform.right, angle.y);
     }
 }
